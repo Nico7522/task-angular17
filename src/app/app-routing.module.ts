c722015664c20f5173a943b882fa67510bf8bb54
Nicolas <nico.daddabbo7100@gmail.com>
2023-12-31 15:38:03 +0100

    début projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'task',
+    loadChildren: () =>
+      import('./components/task/task.module').then((m) => m.TaskModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./components/user/user.module').then((m) => m.UserModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
